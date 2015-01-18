f6ce17858a0a5035f8969bd4bae208720db7b081
Yasuaki Uechi <uetchy@randompaper.co>
2015-01-18 15:34:06 +0900

    Fix bug of encoding

--- lib/typedstream/parser.rb
+++ lib/typedstream/parser.rb
@@ -1,8 +1,6 @@
-# coding: utf-8
-
 class Typedstream::Parser
   def initialize(data)
-    @data = data.force_encoding('utf-8')
+    @data = data.force_encoding("ASCII-8BIT")
   end
 
   def self.parse(data)
@@ -17,32 +15,33 @@ class Typedstream::Parser
     while(cursor < @data.size)
       buffer += @data[cursor]
       case buffer
-      when "\x04\x0B\x73\x74\x72\x65\x61\x6D\x74\x79\x70\x65\x64\x81\xE8\x03\x84\x01\x40\x84"
+      when "\x04\x0B\x73\x74\x72\x65\x61\x6D\x74\x79\x70\x65\x64\x81\xE8\x03\x84\x01\x40\x84".force_encoding("ASCII-8BIT")
         # Header
         buffer = ''
-      when "\x84\x84"
+      when "\x84\x84".force_encoding("ASCII-8BIT")
         # Object
         len = @data[cursor+1].bytes[0]
         objects << @data[cursor+2, len]
         buffer = ''
         cursor += len+2
-      when "\x85\x84\x01"
+      when "\x85\x84\x01".force_encoding("ASCII-8BIT")
         # Value
         kind = @data[cursor+1]
         case kind
-        when "\x2a" # Number
+        when "\x2a".force_encoding("ASCII-8BIT") # Number
           type = @data[cursor+5]
-          if @data[cursor+7] == "\x81"
+          binding.pry
+          if @data[cursor+7] == "\x81".force_encoding("ASCII-8BIT")
             # Little-endian format
             value = @data[cursor+8, @data.size - (cursor+8)].unpack("S*")[0]
           else
             value = @data[cursor+7, @data.size - (cursor+7)].bytes[0]
           end
-        when "\x2b" # String
+        when "\x2b".force_encoding("ASCII-8BIT") # String
           len = @data[cursor+2, 1].bytes[0]
           value = @data[cursor+3, len]
           break
-        when "\x69" # Array
+        when "\x69".force_encoding("ASCII-8BIT") # Array
           type = @data[cursor+5]
           value = @data[cursor+6] || []
           puts "#{objects.first} -> #{value}"
